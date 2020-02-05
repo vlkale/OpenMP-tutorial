fae2a5e11cdd97d4b1a5765e05c24eb72402e8f1
Colleen Bertoni <cebertoni@login1.ascent.olcf.ornl.gov>
2020-02-05 10:09:49 -0500

    clean

--- offload-101/02_target_teams_parallel.cpp
+++ offload-101/02_target_teams_parallel.cpp
@@ -4,11 +4,12 @@
 int main( int argv, char** argc ) {
 
 #pragma omp target teams distribute parallel for simd
-    for(int i=0;i<100000;i++) {
-        printf( "Thread %d out of %d threads in team %d out of %d teams is using index %d\n", 
-		  omp_get_thread_num(), omp_get_num_threads(), 
-		  omp_get_team_num(), omp_get_num_teams(), i );
-}
+  for(int i=0;i<100000;i++)
+    {
+      printf( "Thread %d out of %d threads in team %d out of %d teams is using index %d\n", 
+	      omp_get_thread_num(), omp_get_num_threads(), 
+	      omp_get_team_num(), omp_get_num_teams(), i );
+    }
 
 return 0;
 }
