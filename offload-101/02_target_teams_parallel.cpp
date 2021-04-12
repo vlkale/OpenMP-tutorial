fa4f81c19579f8892b96ed8f153eacb5bfe8cfd4
Colleen Bertoni <cebertoni@login1.ascent.olcf.ornl.gov>
2021-04-12 00:07:21 -0400

    fixing mistake

--- offload-101/02_target_teams_parallel.cpp
+++ offload-101/02_target_teams_parallel.cpp
@@ -2,9 +2,8 @@
 #include <omp.h>
 
 int main( int argv, char** argc ) {
-  // in CUDA <<< numBlocks, blockSize>>>
-#pragma omp target teams distribute parallel for simd num_teams(2) thread_limit(2)
-  for(int i=0;i<4;i++)
+#pragma omp target teams distribute parallel for simd
+  for(int i=0;i<10000;i++)
     {
       printf( "Thread %d out of %d threads in team %d out of %d teams is using index %d\n", 
 	      omp_get_thread_num(), omp_get_num_threads(), 
